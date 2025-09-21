78b96e1401ab64c628f0ff5f9e6df9b192a0ffd5
nick <paul.n.meyer@gmail.com>
2025-09-21 16:21:35 -0700

    Hello world REST end point
    Hit http://localhost:8080/api/data?name=WTF
    
    when running gradle bootRun
    to see the application
    
    Had to upgrade gradle to newer version to make configuration work and upgarde a couple items in the build.gradle

---
+++ README.md
@@ -0,0 +1,17 @@
+# Getting Started
+
+## To build the application
+
+gradle build
+
+## To run the application
+
+gradle bootRun
+
+## Request to the webserver
+
+http://localhost:8080/api/data?name=WTF
+
+Hello, WTF
+
+
---
+++ src/main/java/net/nickmeyer/rest/DataController.java
@@ -0,0 +1,13 @@
+package net.nickmeyer.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+    @GetMapping("/api/data")
+    public String getData(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return "Hello, " + name;
+    }
+}
