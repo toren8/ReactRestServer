e662bb2dc87dc8dd355d7b1bd7c3fc06079548c0
nick <paul.n.meyer@gmail.com>
2025-09-21 16:52:35 -0700

    feat:  Allow development request to my backend
    CORS blocking front-end request.  Fixing for local developmentw

--- src/main/java/net/nickmeyer/rest/DataController.java
+++ src/main/java/net/nickmeyer/rest/DataController.java
@@ -1,9 +1,11 @@
 package net.nickmeyer.rest;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class DataController {
     @GetMapping("/api/data")
